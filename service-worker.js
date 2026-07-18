b3499d84b13143131e2e5803f7919d7e0e391d07
itohen0716 <mail.nori716@gmail.com>
2026-07-18 13:44:08 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="shian-shamisen-v2.0.1";
+const CACHE_NAME="shian-shamisen-v2.1.0";
 const FILES=[
   "./","./index.html","./tuning.html","./style.css","./app.js","./tuner.js",
   "./manifest.webmanifest",
